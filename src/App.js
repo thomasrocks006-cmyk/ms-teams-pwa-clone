ef291852174c4105f14f68a20ecb1ee0489b04af
thomasrocks006-cmyk <thomasrocks006@gmail.com>
2025-08-15 04:58:50 +1000

    Initial scaffold: Teams PWA clone with UI polish and mock data

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Teams PWA Clone</h1>
+      <p>Welcome to your Microsoft Teams PWA clone!</p>
+    </div>
+  );
+}
+
+export default App;
